0ff5cda4c3e6c919596e1faacc462ddad92ec639
edouabou <douaboudev@gmail.com>
2024-03-31 21:45:31 +0000

    edit member done

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, map } from 'rxjs';
 import { Member } from '../_interfaces/member';
 
 @Injectable({
@@ -16,7 +16,14 @@ export class MembersService {
     return this.http.get<any>(this.url)
   }
 
-  getMember(id: string | null): Observable<Member>{
-    return this.http.get<any>(this.url+'/'+id)
+  getMember(id: string | null): Observable<Member | undefined>{
+    return this.getAllMembers().pipe(
+      map(members => members.data.find((member: Member) => member._id === id))
+    );
   }
+  updateMember(member:Member):Observable<Member>{
+    const url = `${this.url}/${member._id}`;
+    return this.http.put<Member>(url,member)  
+  }
+
 }
--- src/app/member/edit/edit.component.ts
+++ src/app/member/edit/edit.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+import { Member } from '../../_interfaces/member';
+import { MembersService } from '../../_services/members.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css'
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
+  member: Member = {
+    _id:"",
+    civilite: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+    user_type: "",
+    status: true,
+    profileUrl: "",
+    __v: 0
+  }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private memberService: MembersService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.getMember()
+      
+  }
+
+  getMember(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.memberService.getMember(id).subscribe(member => {
+      if (member) {
+        this.member = member;
+        console.log("*** de mon component", this.member)
+      } 
+    });
+  }
+
+  saveData(editForm:NgForm){
+    if (editForm.valid) {
+      this.memberService.updateMember(this.member).subscribe(updatedMember => {
+        console.log("Membre mis à jour:", updatedMember);
+        this.router.navigate(['members']);
+        // Vous pouvez ajouter ici une redirection ou un message de confirmation
+      });
+    } 
+    console.log("Les valeurs du formuaire: ",editForm.value);
+    
+    
+  }
 
 }
